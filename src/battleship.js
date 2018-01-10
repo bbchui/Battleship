a29b88dee7fdc4631d9db1f4f7c4adb8930bcfd6
Brandon Chui <bbchui325@gmail.com>
2018-01-10 03:52:09 -0800

    testing canvas

---
+++ src/battleship.js
@@ -0,0 +1,18 @@
+
+
+// let canvas = document.getElementById('canvas')
+// canvas.innerHTML = 'Battleships'
+
+// class Battleship {
+//   constructor() {
+//     this.canvas = document.getElementById('canvas');
+//     this.stage = new createjs.Stage(this.canvas);
+//   }
+//
+//   start() {
+//     this.canvas.innerHTML = 'Battleship'
+//   }
+// }
+//
+// let battleship = new Battleship();
+// battleship.start();
